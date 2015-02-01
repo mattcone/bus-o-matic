2bffab5a0afa8695216d5ee4021fc5f92c4b01c0
Matt Cone <matt@macinstruct.com>
2015-02-01 14:28:11 +0000

    Disable WebMock for the CodeClimate connection

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
 # Let's get coverage reporting from Codeclimate
-WebMock.disable_net_connect!(:allow => "codeclimate.com")
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
@@ -8,6 +7,13 @@ require 'webmock/minitest'
 require 'vcr'
 require "minitest/reporters"
 Minitest::Reporters.use!
+
+# Allow Codeclimate
+WebMock.disable_net_connect!(:allow => "codeclimate.com")
+VCR.configure do |config|
+  # your existing configuration
+  config.ignore_hosts 'codeclimate.com'
+end
 
 #we need the actual library file
 require_relative '../lib/bus-o-matic.rb'
